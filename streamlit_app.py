f96eaeeaeb97811ce3839cc0ebfa0d7c494278bd
Scott <wyf0931@gmail.com>
2023-12-14 07:31:41 +0000

    qianfan

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,51 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import qianfan
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+with st.sidebar:
+    model : str = st.selectbox(
+    '请选择一个模型：',
+    ('🆓XuanYuan-70B-Chat-4bit', '🆓Qianfan-Chinese-Llama-2-13B',
+     '🚌ERNIE-Bot-turbo-AI', '🚌Llama-2-13b-chat','🚌BLOOMZ-7B', 
+     '🚌AquilaChat-7B', '🚌Qianfan-BLOOMZ-7B-compressed', '🚌Llama-2-7b', '🚌ChatGLM2-6B-32K',
+     '🚌Qianfan-Chinese-Llama-2-7B','🚕ERNIE-Bot', '🚕ERNIE-Bot-4',  '🚁Llama-2-70b-chat', '🚁ERNIE-Bot-8k', 
+     '🚀ERNIE-Bot-turbo'))
+    
+    access_key : str = st.text_input("千帆AccessKey :")
+    secret_key : str = st.text_input("千帆SecretKey :")
+    
+    temperature : float = st.number_input("temperature :", 
+                                          help="较高的数值会使输出更加随机，而较低的数值会使其更加集中和确定", 
+                                          min_value=0.0, max_value=1.0, step=0.1, value=0.5)
+    request_timeout : int = st.number_input("请求超时时间（秒）:", min_value=1, max_value=120, value=60)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.title("💡 神灯")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+role_avator = {
+    "assistant": "🤖",
+    "user": "🤔"
+}
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "本宝子知无不言，开始问吧。"}]
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"], avatar=role_avator[msg["role"]]).write(msg["content"])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+if prompt := st.chat_input():
+    if not access_key or secret_key:
+        st.info("请输入百度千帆平台的 AccessKey、SecretKey后访问。")
+        st.stop()
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user", avatar="🤔").write(prompt)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    chat_comp = qianfan.ChatCompletion()
+    # 调用默认模型，即 ERNIE-Bot-turbo
+    resp = chat_comp.do(model=model[1:], 
+                        request_timeout = request_timeout,
+                        temperature=temperature, 
+                        messages=st.session_state.messages[1:])
+    ai_echo = resp.body['result']
+    st.session_state.messages.append({"role": "assistant", "content": ai_echo})
+    st.chat_message("assistant", avatar="🤖").write(ai_echo)
+
